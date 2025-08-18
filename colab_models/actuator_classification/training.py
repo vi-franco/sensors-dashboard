5e580be1cd668b15c88d49d3101c0f5419e780ff
Vincenzo Franco <vincenzo_franco@epam.com>
2025-08-18 16:33:47 +0200

    Improving classification model

--- colab_models/actuator_classification/training.py
+++ colab_models/actuator_classification/training.py
@@ -194,10 +194,10 @@ for fold, (train_idx, val_idx) in enumerate(gkf.split(X_original_df, y_original_
         noise_max=0.02,
     )
 
-    if not aug_df_1.empty or not aug_df_2.empty or not aug_df_3.empty:
-        righe_aggiunte = len(aug_df_1) + len(aug_df_2) + len(aug_df_3)
+    if not aug_df_1.empty or not aug_df_2.empty:
+        righe_aggiunte = len(aug_df_1) + len(aug_df_2)
         print(f"[AUG] Aggiunte {righe_aggiunte} righe (solo training).")
-        final_train_fold = pd.concat([train_fold_original, aug_df_1, aug_df_2, aug_df_3], ignore_index=True)
+        final_train_fold = pd.concat([train_fold_original, aug_df_1, aug_df_2], ignore_index=True)
 
     X_tr, y_tr = final_train_fold[features], final_train_fold[targets].astype(int).values
     X_va, y_va = val_fold[features], val_fold[targets].astype(int).values
